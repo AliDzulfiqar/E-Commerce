a98081071cc4e97d463a8b7a958e2e46855c37dd
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-11 20:19:44 -0800

    Added book info

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,43 +1,45 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from '../components/subcomponents/Rating';
-import Price from '../components/subcomponents/Price';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/subcomponents/Rating";
+import Price from "../components/subcomponents/Price";
 
-const BookInfo = ({books}) => {
-    return (
-        <div id="books__body">
-            <main id="books__main">
-                <div className="books__container">
-                    <div className="row">
-                        <div className="book__selected--top">
-                            <Link to="/books" className="book__link">
-                                <FontAwesomeIcon icon="arrow-left"/>
-                            </Link>
-                            <Link to="/books" className="book__link">
-                                <h2 className="book__selected--title--top">Books</h2>
-                            </Link>
-                        </div>
-                        <div className="book__selected">
-                            <figure className="book__selected--figure">
-                                <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" />
-                            </figure>
-                            <div className="book__selected--description">
-                                <h2 className="book__selected--title">
-                                    Crack the coding interview
-                                </h2>
-                                <Rating rating="4.5"></Rating>
-                                <div className="book__selected">
-                                    <Price salePrice={20.00} originalPrice={40.00}></Price>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+const BookInfo = ({ books }) => {
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img
+                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
+                  alt=""
+                />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">
+                  Crack the coding interview
+                </h2>
+                <Rating rating="4.5"></Rating>
+                <div className="book__selected">
+                  <Price salePrice={20.0} originalPrice={40.0}></Price>
                 </div>
-            </main>
-            
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 };
 
 export default BookInfo;
